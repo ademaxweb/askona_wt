b310be4571dd6b1b850651483caa1db810b9f570
ademaxweb <lopas1902@gmail.com>
2025-03-29 00:32:34 +0300

    Restructured

--- custom_report/backend/search_assessment_appraises.js
+++
@@ -1,8 +0,0 @@
-var _sql  =
-    "select\n" +
-    "    id,\n" +
-    "    name\n" +
-    "from dbo.assessment_appraises\n" +
-    "where lower(name) like '%" + q + "%'"
-
-RESULT = XQuery("sql: " + _sql);
--- custom_report/backend/search_collaborators.js
+++
@@ -1,9 +0,0 @@
-var _sql  =
-    "select\n" +
-    "    id,\n" +
-    "    fullname,\n" +
-    "    position_name\n" +
-    "from dbo.collaborators\n" +
-    "where lower(fullname) like lower('%" + q + "%')";
-
-RESULT = XQuery("sql: " + _sql);
