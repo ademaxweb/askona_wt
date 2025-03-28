26f35eea44d964a19fd3ff075b34dce1579f696b
ademaxweb <lopas1902@gmail.com>
2025-03-29 02:33:35 +0300

    Edited VTable component and poll_report

--- frontend/vue/components/tables/VTable.js
+++ frontend/vue/components/tables/VTable.js
@@ -1,27 +1,50 @@
 const VTable = {
     props: {
-        columns: {
-          type: Array,
-          required: true
-        },
-        rows: {
-          type: Array,
-          required: true
+        table: {
+            type: Object,
+            required: true
         },
         tableId: {
             type: [String, null],
             required: false,
             default: null
+        },
+        baseFileName: {
+            type: [String, null],
+            required: false,
+            default: "table"
         }
     },
 
     setup(props) {
-        const {set_key, sorted: sorted_rows} = useSorter(Vue.toRef(props, "rows"))
+        const rowsRef = Vue.toRef(props.table, "rows");
+        const { set_key, sorted: sorted_rows } = useSorter(rowsRef);
+
+        const exportToExcel = () => {
+            console.log("test")
+
+            if (!props.tableId) return;
+
+            var wb = XLSX.utils.table_to_book(document.getElementById(props.tableId));
+            const date = new Date();
+
+            const formattedDate = date.toLocaleString('ru-RU', {
+                year: 'numeric',
+                month: '2-digit',
+                day: '2-digit',
+                hour: '2-digit',
+                minute: '2-digit',
+                hour12: false
+            }).replace(/[.,\s\/]/g, '_').replace(/_+/g, '_');
+
+            XLSX.writeFile(wb, `${props.baseFileName}_${formattedDate}.xlsx`, { raw: true });
+        };
 
         return {
             set_key,
-            sorted_rows
-        }
+            sorted_rows,
+            exportToExcel
+        };
     },
 
     template: `
@@ -29,18 +52,17 @@ const VTable = {
             <table class="table" :id="tableId">
                 <thead>
                     <tr>
-                    <th v-for="column in columns" @click="set_key(column.key)">
-                        {{column.value}}
-                    </th>
+                        <th v-for="column in table.columns" @click="set_key(column.key)">
+                            {{ column.value }}
+                        </th>
                     </tr>
                 </thead>
-
                 <tbody>
                     <tr v-for="row in sorted_rows">
-                    <td v-for="column in columns">{{ row[column.key] }}</td>
+                        <td v-for="column in table.columns">{{ row[column.key] }}</td>
                     </tr>
                 </tbody>
             </table>
         </div>
     `
-}
+};
